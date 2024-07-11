8803519d7fcc55a229ac3d27264e16d551ce548b
baejimil <baejione@naver.com>
2024-07-11 17:40:39 +0900

    0711상세보기 수정

--- Frontend/src/Pages/QuestionDetail_Page.js
+++ Frontend/src/Pages/QuestionDetail_Page.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nav from '../components/Nav.js';
-import BoardList from '../components/BoardList.js';
+import QuestionDetail  from '../components/QuestionDetail.js'
 import Footer from '../components/Footer.js';
 
 
@@ -8,8 +8,8 @@ export default function Board_Page() {
   return (
     <div>
     <Nav/>
-   <div className="bg-[#17444F] text-white text-center rounded-t-3xl  h-[120vh] flex justify-center items-center">
-      <BoardList/>
+    <div className="bg-[#17444F] text-white text-center rounded-t-3xl  h-[130vh] flex justify-center items-center">
+      <QuestionDetail />
       
     </div>
     <Footer />
